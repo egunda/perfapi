ea6608f192d83861cd2436deb33363ba87878e59
Vivek Sinha Anurag <egunda@users.noreply.github.com>
2024-08-08 21:45:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!isset($_GET['host'])) {
 $host = escapeshellarg($_GET['host']);
 $serverHostname = gethostname();
 
-$command = "curl -s -w \"{ \\\"hostname\\\": \\\"$serverHostname\\\",\\\"dns_resolution\\\": %{time_namelookup},\\\"dns_resolution\\\": %{time_namelookup}, \\\"tcp_established\\\": %{time_connect}, \\\"ssl_handshake_done\\\": %{time_appconnect}, \\\"time_pretransfer\\\": %{time_pretransfer}, \\\"time_redirect\\\": %{time_redirect}, \\\"TTFB\\\": %{time_starttransfer}, \\\"time_total\\\": %{time_total} }\" -o /dev/null $host -I";
+$command = "curl -s -w \"{ \\\"source\\\": \\\"$serverHostname\\\", \\\"target\\\": \\\"$host\\\", \\\"time_namelookup\\\": %{time_namelookup}, \\\"time_connect\\\": %{time_connect}, \\\"time_appconnect\\\": %{time_appconnect}, \\\"time_pretransfer\\\": %{time_pretransfer}, \\\"time_redirect\\\": %{time_redirect}, \\\"time_starttransfer\\\": %{time_starttransfer}, \\\"time_total\\\": %{time_total} }\" -o /dev/null $host -I";
 
 $output = shell_exec($command);
 
